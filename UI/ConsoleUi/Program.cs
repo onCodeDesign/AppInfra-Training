278e3dcbeeb803a748f7f6ea491f8c8977b1878e
Florin Coros <florin@oncodedesign.com>
2025-10-02 10:14:21 +0300

    Boot the console app

--- UI/ConsoleUi/Program.cs
+++ UI/ConsoleUi/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AppBoot;
+using AppBoot.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+Console.WriteLine("Booting up...");
+
+IBootstrapper bootstrapper = null!;
+
+Host.CreateDefaultBuilder(args).ConfigureServices(services =>
+{
+	bootstrapper = services.AddAppBoot(options =>
+	{
+		options.NameFilter = assembly => assembly.StartsWith("Common")
+		                                 || assembly.StartsWith("Contracts")
+		                                 || assembly.StartsWith("DataAccess")
+		                                 || assembly.StartsWith("iQuarc.DataAccess")
+		                                 || assembly.StartsWith("Export.")
+		                                 || assembly.StartsWith("Notifications.")
+		                                 || assembly.StartsWith("Sales.")
+			;
+
+	})
+	.AddRegistrationBehavior(new ServiceRegistrationBehavior())
+	.Run();
+})
+.Build()
+.InitHostedApp(bootstrapper);
+
+Console.WriteLine("AppBoot done!");
