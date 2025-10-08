fcd72db28d5169a2858ff6b1b3c86d031064a806
Florin Coros <florin@oncodedesign.com>
2025-10-08 22:14:48 +0300

    5.1. Implement a `IStateChangeSubscriber<SalesOrderHeader>

---
+++ Modules/Contracts/Sales/CustomerOrders/CustomerData.cs
@@ -0,0 +1,9 @@
+﻿namespace Contracts.Sales.CustomerOrders
+{
+    public class CustomerData
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? SalesPerson { get; set; }
+    }
+}
