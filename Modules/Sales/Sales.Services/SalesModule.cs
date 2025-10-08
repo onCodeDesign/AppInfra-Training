d0278699e8264f47eda27e475760bdec827f762c
Florin Coros <florin@oncodedesign.com>
2025-10-08 12:14:01 +0300

    1.1 Sales Services Module

---
+++ Modules/Sales/Sales.Services/SalesModule.cs
@@ -0,0 +1,16 @@
+﻿using AppBoot;
+using AppBoot.DependencyInjection;
+using Contracts.Notifications;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Sales.Services;
+
+[Service(typeof(IModule), ServiceLifetime.Singleton)]
+class SalesModule(INotificationService notificationService) : IModule
+{
+    public void Initialize(IHost host)
+    {
+        notificationService.NotifyAlive(this);
+    }
+}
