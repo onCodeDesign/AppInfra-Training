6c44c2e74ebf8ecd79b1e3acae664cb40b83783c
Florin Coros <florin@oncodedesign.com>
2025-11-07 17:04:21 +0200

    Lab1 - section 1.2. Change the order in which the modules are initialized in a loosely coupled way

--- Modules/Sales/Sales.Services/SalesServicesModule.cs
+++ Modules/Sales/Sales.Services/SalesServicesModule.cs
@@ -3,9 +3,11 @@ using AppBoot.DependencyInjection;
 using Contracts.Notifications;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AppBoot.SystemEx.Priority;
 
 namespace Sales.Services;
 
+[Priority(Priorities.High)]
 [Service(typeof(IModule), ServiceLifetime.Singleton)]
 class SalesServicesModule(INotificationService notificationService) : IModule
 {
